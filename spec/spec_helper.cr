0d5d937034bcfbce61203551a5ab083bada40a30
Russ Smith <russ@bashme.org>
2025-08-14 12:40:44 -0700

    An initial spec setup for getting things going.
    No assertions yet. I need to probably put a database in play to see if
    jobs are being ran in specs.

---
+++ spec/spec_helper.cr
@@ -0,0 +1,13 @@
+require "log"
+
+Log.setup_from_env(default_level: :error)
+
+Signal::SEGV.reset # Let the OS generate a coredump
+
+require "spec"
+require "../src/tj_lavin"
+require "./support/*"
+
+TJLavin.configure do |settings|
+  settings.amqp_url = ENV["AMQP_URL"]
+end
